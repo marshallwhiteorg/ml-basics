531dc8b26dd935f283e361c2f52bdf2845d4c45b
marshall <mw@marshallwhite.org>
2021-02-19 22:48:51 -0500

    Add MNIST data, add Nielsen's data loading code and update it to work with python 3, add and update Nielsen's backprop code.

---
+++ mnist_loader.py
@@ -0,0 +1,68 @@
+# derived from https://github.com/mnielsen/neural-networks-and-deep-learning
+
+import pickle
+import gzip
+import numpy as np
+
+def _load_data() -> (np.ndarray, np.ndarray, np.ndarray):
+    ''' Returns the MNIST data as a tuple containing the training data,
+    the validation data, and the test data.
+
+    training_data is a tuple with two entries.
+    The first entry is an ndarray of length 50,000 where each entry is an
+    ndarray with 784 values, representing the pixels in one MNIST image.
+    The second entry is an ndarray containing the 50,000 labels, where each
+    label is a digit 0...9.
+
+    validation_data and test_data have the same structure, except the lengths
+    are 10,000.
+    '''
+    f = gzip.open('./data/mnist.pkl.gz', 'rb')
+    # There is some issue with the encoding of the file, but latin1 seems
+    # to work.
+    training_data, validation_data, test_data = pickle.load(
+        f, encoding='latin1')
+    f.close()
+    return (training_data, validation_data, test_data)
+
+def load_data_wrapper() -> (np.ndarray, np.ndarray, np.ndarray):
+    ''' Returns a tuple of MNIST data as (training_data, validation_data,
+    test_data). Like load_data, but the format is altered for easier use.
+
+    In particular, training_data is a list containing 50,000
+    (x, y) pairs. x is a 784-dimensional ndarray
+    containing the input image. y is a 10-dimensional
+    ndarray representing the unit vector corresponding to the
+    correct digit for x.
+
+    validation_data and test_data are lists containing 10,000
+    (x, y) pairs.  In each case, x is a 784-dimensional
+    ndarray containing the input image, and y is the
+    corresponding classification, i.e., the digit
+    corresponding to x.
+    
+    Note the difference in format between the lists. training_data
+    encodes the digit as a 10d vector with a 1.0 in the digit's position,
+    and validation_data and test_data encode the digit as an integer.
+    Additionally, in all three cases x has shape (784, 1), not (784,).
+    Apparently this is convenient for use.
+    '''
+    tr_d, va_d, te_d = _load_data()
+    training_inputs = [np.reshape(x, (784, 1)) for x in tr_d[0]]
+    training_results = [_vectorized_result(y) for y in tr_d[1]]
+    training_data = list(zip(training_inputs, training_results))
+    validation_inputs = [np.reshape(x, (784, 1)) for x in va_d[0]]
+    validation_data = list(zip(validation_inputs, va_d[1]))
+    test_inputs = [np.reshape(x, (784, 1)) for x in te_d[0]]
+    test_data = list(zip(test_inputs, te_d[1]))
+    return (training_data, validation_data, test_data)
+
+def _vectorized_result(j):
+    """Return a 10-dimensional unit vector with a 1.0 in the jth
+    position and zeroes elsewhere.  This is used to convert a digit
+    (0...9) into a corresponding desired output from the neural
+    network."""
+    e = np.zeros((10, 1))
+    e[j] = 1.0
+    return e
+
--- network1.py
+++ network1.py
@@ -7,7 +7,13 @@ WILL_IMPLEMENT_TOMORROW = '''print("Will implement tomorrow.")'''
 
 
 def sigmoid(z: npt.ArrayLike):
-    return 1/(1 + np.exp(-1 * np.asarray(z)))
+    ''' The sigmoid function. '''
+    z = np.asarray(z)
+    return 1/(1+np.exp(-z))
+
+def sigmoid_prime(z: npt.ArrayLike):
+    ''' Derivative of the sigmoid function. '''
+    return sigmoid(z)*(1-sigmoid(z))
 
 
 class Network:
@@ -66,7 +72,8 @@ class Network:
             mini_batches = [training_data[k:k+mini_batch_size]
                             for k in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
-                self.update_params(mini_batch, lr)
+                #self.update_params(mini_batch, lr)
+                self.update_mini_batch(mini_batch, lr)
             if validation_data:
                 n_correct = self.evaluate(validation_data)
                 print("Epoch {0}: {1} / {2}".format(
@@ -82,10 +89,10 @@ class Network:
         mini_batch: array of (x, y) pairs of training data
         lr: learning rate
         '''
-        exec(WILL_IMPLEMENT_TOMORROW)
+        pass
 
 
-    def evaluate(self, data: np.ndarray) -> int:
+    def my_evaluate(self, data: np.ndarray) -> int:
         ''' Returns the number of correct predictions by the network.
         
         Performs a forward pass through the network for each example
@@ -94,4 +101,70 @@ class Network:
 
         data: array of (x, y) pairs
         '''
-        exec(WILL_IMPLEMENT_TOMORROW)
+        pass
+        
+
+    def update_mini_batch(self, mini_batch, eta):
+        """Update the network's weights and biases by applying
+        gradient descent using backpropagation to a single mini batch.
+        The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
+        is the learning rate."""
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w-(eta/len(mini_batch))*nw
+                        for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(eta/len(mini_batch))*nb
+                       for b, nb in zip(self.biases, nabla_b)]
+
+    def backprop(self, x, y):
+        """Return a tuple ``(nabla_b, nabla_w)`` representing the
+        gradient for the cost function C_x.  ``nabla_b`` and
+        ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
+        to ``self.biases`` and ``self.weights``."""
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        # feedforward
+        activation = x
+        activations = [x] # list to store all the activations, layer by layer
+        zs = [] # list to store all the z vectors, layer by layer
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation)+b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+        # backward pass
+        delta = self.cost_derivative(activations[-1], y) * \
+            sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        # Note that the variable l in the loop below is used a little
+        # differently to the notation in Chapter 2 of the book.  Here,
+        # l = 1 means the last layer of neurons, l = 2 is the
+        # second-last layer, and so on.  It's a renumbering of the
+        # scheme in the book, used here to take advantage of the fact
+        # that Python can use negative indices in lists.
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+        return (nabla_b, nabla_w)
+
+    def evaluate(self, test_data):
+        """Return the number of test inputs for which the neural
+        network outputs the correct result. Note that the neural
+        network's output is assumed to be the index of whichever
+        neuron in the final layer has the highest activation."""
+        test_results = [(np.argmax(self.feedforward(x)), y)
+                        for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
+
+    def cost_derivative(self, output_activations, y):
+        """Return the vector of partial derivatives \partial C_x /
+        \partial a for the output activations."""
+        return (output_activations-y)
